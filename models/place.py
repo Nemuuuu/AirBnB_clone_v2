9525035baf0132c5d7e881d3fab3473778b88642
Neima Nesru <neimanesru34@gmail.com>
2023-03-22 00:28:21 -0700

    models updated

--- models/place.py
+++ models/place.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel, Base
+from base_model import BaseModel, Base
 from sqlalchemy import Column, Integer,Table, Float, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
