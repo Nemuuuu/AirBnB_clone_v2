b42721cb8158bc8c775ca0172d8386a77d213327
Neima Nesru <neimanesru34@gmail.com>
2023-03-21 22:52:21 -0700

    state file updated

--- models/state.py
+++ models/state.py
@@ -11,7 +11,7 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
 
     if getenv('HBNB_TYPE_STORAGE') == 'db': # DB storage
-        cities = relationship('City', backref='state' cascase='all, delete')
+        cities = relationship('City', backref='state', cascase='all, delete')
     else: # file storage
         @property
         def cities(self):
