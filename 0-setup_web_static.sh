d34af37323cc6162ffbd575a4e45c5ca067d3257
Neima Nesru <neimanesru34@gmail.com>
2023-04-05 05:39:53 -0700

    webserver setup

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# script to setup web servers for the deployment of web static
+
+#install nginx
+sudo apt-get update -y
+sudo apt-get install nginx -y
+
+#create folders
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkidr -p /data/web_static/shared/
+
+#create html file with fake content
+echo "
+<!DOCTYPE html>
+<html lang="en">
+        <head>
+                <meta charset="utf-8">
+                <title>Document</title>
+        </head>
+        <body>
+                <h1>Welcome to our Page</h1>
+        </body>
+
+</html>
+"  sudo /data/web_static/releases/test/index.html
+
+#create symblic link
+sudo rm /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+#give ownership to ubuntu user and group recursively
+sudo chown -R ubuntu:ubuntu /data/
+
+#setup nginx
+sudo sed -i '/listen 80 default_server/a location /hbnb_static {
+                alias /data/web_static/current;
+                index index.html;
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a >
+                try_files $uri $uri/ =404;
+        }' /etc/nginx/sites-available/default
+
+# restart nginx
+sudo service nginx restart
