7ca66dcb53aff031e75dea8addf5d81fc72b3163
Neima Nesru <neimanesru34@gmail.com>
2023-04-06 06:18:19 -0700

    compressing web static files

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,20 @@
-#!usr/bin/python3
-""" helps to compress files using fabric library """
-from fabric.api import *
+#!/usr/bin/python3
+
+"""Fabric script to compress web_static in archive"""
+
+from fabric.api import local
 from datetime import datetime
-import os
 
 
 def do_pack():
-    """ compressing files in archive """
-    now = datetime.now().strftime('%Y%m%d%H%M%S')
-    local('mkdir -p versions')
-    file_name = 'web_static_{}.tgz'.format(now)
-    archive_path = os.path.join("versions", file_name)
-    check = local('tar -cvzf {} web_static'.format(archive_path))
-    if check.failed:
-        return None
-    else:
-        return archive_path
+
+    """Create an archive from the contents of web_static folder."""
+
+    now = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    path = "versions/web_static_{}.tgz".format(now)
+    try:
+        local('mkdir -p versions')
+        local('tar -cvzf {} web_static'.format(path))
+        return path
+    except:
+        return none
