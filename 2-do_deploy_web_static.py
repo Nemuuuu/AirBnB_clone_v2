88d9cc8b4992ed0585a2af6fb2392aba944a33c3
Neima Nesru <neimanesru34@gmail.com>
2023-04-06 06:38:10 -0700

    deploying

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,31 +1,46 @@
 #!/usr/bin/python3
-"""
-Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers 
-"""
-from fabric.api import env, put, run, sudo
-from os.path import exists
+
+"""Fabric script to deploy web_static to web servers."""
+
+from fabric.api import run, put, env, local
+from datetime import datetime
 import os
 
 env.hosts = ['34.207.188.143', '100.25.160.228']
 
-def do_deploy(archive_path):
-    """ deploy """
-    if not os.path.exists(archive_path):
-        return False
-    data_path = '/data/web_static/releases/'
-    tmp = archive_path.split('.')[0]
-    name = tmp.split('/')[1]
-    dest = data_path + name
+
+def do_pack():
+
+    """Create an archive from the contents of web_static folder."""
+
+    now = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    path = "versions/web_static_{}.tgz".format(now)
     try:
-        put(archive_path, '/tmp')
-        run('mkdir -p {}'.format(dest))
-        run('tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
-        run('rm -f /tmp/{}.tgz'.format(name))
-        run('mv {}/web_static/* {}/'.format(dest, dest))
-        run('rm -rf {}/web_static'.format(dest))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(dest))
+        local('mkdir -p versions')
+        local('tar -cvzf {} web_static'.format(path))
+        return path
+    except:
+        return none
+
+
+def do_deploy(archive_path):
+    """Distribute the archive to the web servers."""
+
+    if archive_path is None or os.path.isfile(archive_path) is False:
+        return False
+
+    try:
+        a_name = archive_path.split('/')[-1]
+        put('{}'.format(archive_path), '/tmp/{}'.format(a_name))
+        name_target = a_name.split('.')[0]
+        full_target = "/data/web_static/releases/{}".format(name_target)
+        run('mkdir -p {}'.format(full_target))
+        run("tar -xzf /tmp/{} -C {}".format(a_name, full_target))
+        run('rm /tmp/{}'.format(a_name))
+        run('mv {}/web_static/* {}/'.format(full_target, full_target))
+        run('rm -Rf {}/web_static'.format(full_target))
+        run('rm -Rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(full_target))
         return True
     except:
         return False
-
