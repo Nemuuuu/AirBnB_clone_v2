1a419761030f55f89263cd3b68bf6568769188d0
Neima Nesru <neimanesru34@gmail.com>
2023-04-05 19:39:44 -0700

    deploay

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,19 +9,21 @@ import os
 env.hosts = ['34.207.188.143', '100.25.160.228']
 
 def do_deploy(archive_path):
-    if not exists(archive_path):
+    if not os.path.exists(archive_path):
         return False
+    data_path = '/data/web_static/releases/'
+    tmp = archive_path.split('.')[0]
+    name = tmp.split('/')[1]
+    dest = data_path + name
     try:
-        filename = os.path.basename(archive_path)
-        dirname = os.path.splitext(filename)[0]
-
         put(archive_path, '/tmp')
-
-        run('mkdir -p /data/web_static/releases/{}'.format(dirname))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(filename, dirname))
-        run('rm /tmp/{}'.format(filename))
-        run('sudo rm -f /data/web_static/current')
-        run('sudo ln -s /data/web_static/releases/{}//data/web_static/current'.format(dirname))
+        run('mkdir -p {}'.format(dest))
+        run('tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
+        run('rm -f /tmp/{}.tgz'.format(name))
+        run('mv {}/web_static/* {}/'.format(dest, dest))
+        run('rm -rf {}/web_static'.format(dest))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(dest))
         return True
     except:
         return False
