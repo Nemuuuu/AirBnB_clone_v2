5644659140e0cf2a9cbbad2993d5d2c3d9711bbe
Neima Nesru <neimanesru34@gmail.com>
2023-04-22 01:03:33 -0700

    starting flsk app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-from flask import Flask
 """a script that starts a Flask web application"""
-app=Flask(__name__)
-@app.route('/', strict_slashes=False)
+from flask import Flask
+app = Flask(__name__)
+app.strict_slashes = False
+
+@app.route('/')
 def hello_hbnb():
     """ a method to display Hello HBNB!"""
     return "Hello HBNB!"
