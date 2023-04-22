d48be6284146c4d6cf3d9c4847f62ffd142118e1
Neima Nesru <neimanesru34@gmail.com>
2023-04-22 01:32:50 -0700

    2nd task

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+app.strict_slashes=False
+
+@app.route('/')
+def hello():
+    """ display Hello HBNB! """
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    """ display HBNB"""
+    return "HBNB"
+
+@app.route('/c/<text>')
+def c(text):
+    """display “C ” followed by the value of the text variable (replace underscore _ symbols with a space"""
+    text = text.replace('_', '')
+    return 'c {}'.format(text)
+
