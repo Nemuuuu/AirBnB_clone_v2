d47d9580ef715a5fab17447774503dedb042dfdb
Neima Nesru <neimanesru34@gmail.com>
2023-04-22 03:11:47 -0700

    updated

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,5 +21,4 @@ def c(text):
     return 'C {}'.format(text)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -28,5 +28,4 @@ def python(text="is cool"):
     return 'Python {}'.format(text)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(host='0.0.0.0', port=5000, debug=True)
