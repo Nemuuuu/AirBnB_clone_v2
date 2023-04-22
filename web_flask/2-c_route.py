c0938f94ee595f27e32b575a638ee99802f4e829
Neima Nesru <neimanesru34@gmail.com>
2023-04-22 01:40:12 -0700

    updated

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,7 +18,7 @@ def hbnb():
 def c(text):
     """display “C ” followed by the value of the text variable (replace underscore _ symbols with a space"""
     text = text.replace('_', ' ')
-    return 'c {}'.format(text)
+    return 'C {}'.format(text)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
