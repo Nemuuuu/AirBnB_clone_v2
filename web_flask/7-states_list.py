68216178d427679c36f109abef8f19eea90aa96a
Neima Nesru <neimanesru34@gmail.com>
2023-04-23 02:30:45 -0700

    state lists

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" a script that starts a Flask web application """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.strict_slashes = False
+
+@app.teardown_appcontext
+def close_DB(self):
+    """ remove current session """
+    storage.close()
+
+@app.routes('/states_list')
+def state_lists():
+    """ displays all lists present in DB storage"""
+    return render_template('7-states_list.html',
+                           state=storage.all(State).values())
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
